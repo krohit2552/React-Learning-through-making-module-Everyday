b2f1b24c4c6491e3fd7b803757acf83369f330b4
krohit2552 <kumarrohit2551997@gmail.com>
2025-07-05 14:22:12 +0530

    callback using ecom website

---
+++ 08/03.js
@@ -0,0 +1,17 @@
+
+function saveOrderToDB(orderdata, callback){
+    setTimeout(()=>{
+        console.log("order save in database");
+        const message = callback();
+        document.getElementById("demo").innerHTML = message;
+    },2000);
+
+}
+
+function sendConfirmationEmail(){
+    console.log("confirmation email sent to customer");
+    return "your order is confirm";
+}
+
+
+saveOrderToDB({item:"mobile apple"}, sendConfirmationEmail);
